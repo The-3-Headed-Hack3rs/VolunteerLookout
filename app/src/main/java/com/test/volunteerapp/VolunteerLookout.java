b9f8214ccb907952e9e208ba2fbf07b109b13790
TheNinjaKid7 <gvaldez@bulldogs.aamu.edu>
2020-04-29 13:43:21 -0400

    second update part 2

--- app/src/main/java/com/test/volunteerapp/VolunteerLookout.java
+++ app/src/main/java/com/test/volunteerapp/VolunteerLookout.java
@@ -1,13 +1,46 @@
 package com.test.volunteerapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class VolunteerLookout extends AppCompatActivity {
+
+
+   public Button CreateAccBtn;
+
+
+    public void init() {
+
+        CreateAccBtn = (Button) findViewById(R.id.CreateAccBtn);
+
+        CreateAccBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent toy = new Intent(VolunteerLookout.this,NewAccount.class);
+
+                startActivity(toy);
+
+            }
+        });
+
+
+
+}
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_volunteer_lookout);
+
+        init();
+
     }
 }
